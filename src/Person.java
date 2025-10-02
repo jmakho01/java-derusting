bf9e1ca33ba2759c1269d3a8db1fa06f4eeeb55b
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-10-01 21:20:45 -0700

    Finished Person.java

--- src/Person.java
+++ src/Person.java
@@ -6,14 +6,22 @@
 public class Person {
   // Declare a public String instance variable for the name of the person
   // Declare a private int instance variable for the age of the person
-
+  public String name;
+  private int age;
 
   // Create a constructor that takes the name and age of the person
   // and assigns it to the instance variables
-
+  public Person(String n, int a)
+  {
+    name = n;
+    age = a;
+  }
 
   // Create a toString method that gives the name and age of the person
-
+  public String toString()
+  {
+    return name + " " + age;
+  }
 
   // Implement the below public instance method "birthYear"
   // There should NOT be any print statement in this method.
@@ -28,26 +36,30 @@ public class Person {
    * @return The year the person was born
    */
   // (create the instance method here)
-
+  public int birthYear(int currentYear)
+  {
+    return currentYear - age; 
+  }
 
   public static void main(String[] args) {
     // Create an instance of Person
-
+    Person thePerson = new Person("Alex", 24); 
     // Create another instance of Person with a different name and age and
     // assign it to a different variable
-
+    Person anotherPerson = new Person("Marvin", 21);
     // Print the first person
-
+    System.out.println(thePerson.toString());
     // Print the second person
-
+    System.out.println(anotherPerson.toString());
     // Get the name of the first person and store it in a local variable
-
+    String aName = thePerson.name;
+    System.out.println(aName);
     // Using the birthYear method, get the birth year of the first person
     // and store it in a local variable. Input the actual current year (e.g. 2025)
     // as the argument.
-
+    int firstBYear = thePerson.birthYear(2025);
     // In a separate statement, print the local variable holding the birth year.
-
+    System.out.println(firstBYear);
     /**
      * Terminology!
      * 
